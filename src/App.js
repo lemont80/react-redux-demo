0109c9f082283bd382d335a841758adfe4627203
Lloyd Randolf Montifalcon <lloyd.montifalcon@gmail.com>
2023-11-21 08:11:20 +0900

    update

--- src/App.js
+++ src/App.js
@@ -6,15 +6,22 @@ import CakeContainer from './components/CakeContainer';
 import HooksCakeContainer from './components/HooksCakeContainer';
 import IceCreamContainer from './components/IceCreamContainer';
 import HooksIceCreamContainer from './components/HooksIceCreamContainer';
+import NewCakeContainer from './components/NewCakeContainer';
+import ItemContainer from './components/ItemContainer';
+import UserContainer from './components/UserContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        {/* <ItemContainer cake />
+        <ItemContainer />
         <HooksCakeContainer />
         <CakeContainer />
         <HooksIceCreamContainer />
         <IceCreamContainer />
+        <NewCakeContainer /> */}
+        <UserContainer />
       </div>
     </Provider>
   );
